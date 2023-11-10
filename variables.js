5fe5575dfc8b2cc384d47db93f5334f01a634228
Ahtsham-Jabbar <150022732+Ahtsham-Jabbar@users.noreply.github.com>
2023-11-10 14:06:51 +0000

    clear

--- variables.js
+++ variables.js
@@ -1,7 +1,9 @@
 const StudentId = 12344
 let StudentEmail = "sample@mail.com"
 let studentPassword = "123456"
-let studentCity + "Lahore"
+let studentCity = "Lahore"
+let studentState;
 
 
-console.log("StudentId");
+// console.log("StudentId");
+console.table([ StudentId, StudentEmail, studentPassword, studentCity, studentState ])
